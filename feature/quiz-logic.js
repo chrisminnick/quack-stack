c0032ac0c87c1474371b8341af2513180c34d7f8
Lavanya Goel <lavanya.goel12@gmail.com>
2025-02-28 13:48:53 -0500

    added quiz questions

--- feature/quiz-logic.js
+++ feature/quiz-logic.js
@@ -19,6 +19,51 @@ const questions = [
     options: ["Mallard", "Penguin", "Eagle", "Sparrow"],
     answer: "Mallard",
   },
+  {
+    question: "How many species of duck are there?",
+    options: ["250", "150", "80", "60"],
+    answer: "150",
+  },
+  {
+    question: "What is a male duck called?",
+    options: ["Mallard", "Mandible", "Drake", "Hen"],
+    answer: "Drake",
+  },
+  {
+    question: "Up to what speed can ducks fly?",
+    options: ["50mph", "70mph", "90mph", "100mph"],
+    answer: "100mph",
+  },
+  {
+    question: "What differentiates the duck from most other birds?",
+    options: [
+      "Totally monogamous",
+      "Flies long distances",
+      "Swims well",
+      "Size",
+    ],
+    answer: "Swims well",
+  },
+  {
+    question: "Where will you not find ducks?",
+    options: ["Asia", "Africa", "Australia", "Antarctica"],
+    answer: "Antarctica",
+  },
+  {
+    question: "Which duck is the only duck to nest in trees?",
+    options: ["Mallard", "Wood duck", "Gadwell", "Redhead"],
+    answer: "Wood duck",
+  },
+  {
+    question: "Which is a significant part of a duck's diet?",
+    options: [
+      "Grasses and seeds",
+      "Seeds and worms",
+      "Fish and aquatic vegetation",
+      "Insects and invertebrates",
+    ],
+    answer: "Insects and invertebrates",
+  },
 ];
 
 let currentQuestionIndex = 0;
