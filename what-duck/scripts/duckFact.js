98f47d305565c8414c0855a1bec391fb41513d18
Alice Smith <alice@webage.com>
2025-02-28 15:57:59 -0500

    final website

--- what-duck/scripts/duckFact.js
+++ what-duck/scripts/duckFact.js
@@ -1,11 +1,11 @@
 async function getFact() {
   try {
-    const response = await fetch(`../duckFacts.json`);
+    const response = await fetch("duckFacts.json");
     const facts = await response.json();
 
     const randomFact = facts[Math.floor(Math.random() * facts.length)];
-
-    console.log("Random fact: ", randomFact);
+    console.log(randomFact);
+    document.getElementById("fact").innerText = randomFact.fact;
   } catch (error) {
     console.error("Error: ", error);
   }
